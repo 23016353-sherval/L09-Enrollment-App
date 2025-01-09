c474e9f0c3cf61c20087cf7c3acc48df5087a20c
23016353-sherval <23016353@myrp.edu.sg>
2025-01-09 23:12:03 +0800

    Finish Problem Statement

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -5,12 +5,12 @@ export default function Confirmation() {
 
   return (
     <div className="container">
-      <h1>Thank You!</h1>
+      <h1>Thank You, <strong>{state.name}</strong>!</h1>
       {state && (
         <>
-          <p>{state.name}, You're now registered for Red30 Tech.</p>
+          <p>You're now registered for <strong>{state.course}</strong>.</p>
           <p>
-            We've sent more details to <strong>{state.email}</strong>
+            We've emailed more details to you at <strong>{state.email}</strong>
           </p>
         </>
       )}
